bc0ed551a910972aa61767795d77cdb3f0638eb3
Cédric Meuter <cedric.meuter@gmail.com>
2023-03-24 07:39:10 +0100

    Changed the colors of the `fancy_cwd` and `fancy_branch` component
    - `fancy_cwd`: the icon is now AliceBlue to match the folder icon
      in Neotree. The foreground color for the text is now nore legible.
    - `fancy_branch`: the icon for the the component has changed to a red
      branch in a square icon.

--- lua/lualine/components/fancy_branch.lua
+++ lua/lualine/components/fancy_branch.lua
@@ -1,7 +1,7 @@
 local M = require("lualine.components.branch"):extend()
 
 function M:init(options)
-    options.icon = options.icon or ""
+    options.icon = options.icon or { "", color = { fg = "red" } }
     M.super.init(self, options)
 end
 
