f311ec2975ba231aabf1741b5145e8606da77cbd
Cédric Meuter <cedric.meuter@gmail.com>
2023-03-24 09:47:25 +0100

    Added `fancy_location` component

---
+++ lua/lualine/components/fancy_location.lua
@@ -0,0 +1,14 @@
+local M = require("lualine.component"):extend()
+
+function M:init(options)
+    options.icon = options.icon or { "󰍒", color = { fg = "#fc5603" }, align = "right" }
+    M.super.init(self, options)
+end
+
+function M:update_status()
+    local line = vim.fn.line(".")
+    local col = vim.fn.virtcol(".")
+    return string.format('%d:%d', line, col)
+end
+
+return M
