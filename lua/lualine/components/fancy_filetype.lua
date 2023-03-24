88b697150f4bfd786f39049bf40a1933f5050780
Cédric Meuter <cedric.meuter@gmail.com>
2023-03-24 08:51:25 +0100

    Changed the default color of the treesitter icon in `fancy_filetype`

--- lua/lualine/components/fancy_filetype.lua
+++ lua/lualine/components/fancy_filetype.lua
@@ -2,15 +2,34 @@ local M = require("lualine.components.filetype"):extend()
 
 function M:init(options)
     options.ts_icon = options.ts_icon or ""
+    if type(options.ts_icon) == "string" then
+        options.ts_icon = { options.ts_icon }
+    end
+
+    options.ts_icon = vim.tbl_deep_extend("keep", options.ts_icon, {
+        color = { fg = "#41942c" },
+        align = "right"
+    })
+
     M.super.init(self, options)
+
+    self.ts_icon_hl = self:create_hl(self.options.ts_icon.color, "ts_icon")
 end
 
-function M:update_status()
-    local result = M.super.update_status(self)
+function M:apply_icon()
+    M.super.apply_icon(self)
+
     if vim.treesitter.highlighter.active[vim.api.nvim_get_current_buf()] == nil then
-        return result
+        return
     end
-    return result .. " " .. self.options.ts_icon
+
+    local icon = self:format_hl(self.ts_icon_hl) .. self.options.ts_icon[1]
+    local align = self.options.ts_icon.align
+    if align == "left" then
+        self.status = icon .. " " .. self.status
+    else
+        self.status = self.status .. " " .. icon
+    end
 end
 
 return M
