53d2095e1935e8721d884698d486b34cd5758810
Cédric Meuter <cedric.meuter@gmail.com>
2023-04-04 19:50:48 +0200

    changed default color of git icon in  to match icon color of

--- lua/lualine/components/fancy_branch.lua
+++ lua/lualine/components/fancy_branch.lua
@@ -1,7 +1,7 @@
 local M = require("lualine.components.branch"):extend()
 
 function M:init(options)
-    options.icon = options.icon or { "", color = { fg = "red" } }
+    options.icon = options.icon or { "", color = { fg = "#fc5603" } }
     M.super.init(self, options)
 end
 
