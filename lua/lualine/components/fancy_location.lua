8bf55799eb1ccbf2cb8a14a7c70498708d2183c9
greeid <deekshit.bti@gmail.com>
2024-07-13 23:22:51 +0545

    `chore` : move location icon to left

--- lua/lualine/components/fancy_location.lua
+++ lua/lualine/components/fancy_location.lua
@@ -1,14 +1,14 @@
 local M = require("lualine.component"):extend()
 
 function M:init(options)
-    options.icon = options.icon or { "󰍒", color = { fg = "#fc5603" }, align = "right" }
-    M.super.init(self, options)
+	options.icon = options.icon or { "󰍒", color = { fg = "#fc5603" }, align = "left" }
+	M.super.init(self, options)
 end
 
 function M:update_status()
-    local line = vim.fn.line(".")
-    local col = vim.fn.virtcol(".")
-    return string.format('%d:%d', line, col)
+	local line = vim.fn.line(".")
+	local col = vim.fn.virtcol(".")
+	return string.format("%d:%d", line, col)
 end
 
 return M
