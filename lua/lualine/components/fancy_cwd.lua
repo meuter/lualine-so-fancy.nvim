0b0a605481cf4c2a9c530b1dfb108fceb22db148
greeid <deekshit.bti@gmail.com>
2024-07-20 21:26:01 +0545

    `chore(refac)` : refactored how cwd looks

--- lua/lualine/components/fancy_cwd.lua
+++ lua/lualine/components/fancy_cwd.lua
@@ -1,22 +1,27 @@
 local M = require("lualine.component"):extend()
 
 function M:init(options)
-    options.icon = options.icon or { "", color = { fg = "SkyBlue3" } }
-    if options.substitute_home == nil then
-        options.substitute_home = true
-    end
-    M.super.init(self, options)
+	options.icon = options.icon or { "󰉖" }
+	if options.substitute_home == nil then
+		options.substitute_home = true
+	end
+	M.super.init(self, options)
 end
 
+-- function M:update_status()
+-- 	local result = vim.fn.getcwd()
+-- 	if self.options.substitute_home then
+-- 		local home = os.getenv("HOME")
+-- 		if home and vim.startswith(result, home) then
+-- 			result = "~" .. result:sub(home:len() + 1)
+-- 		end
+-- 	end
+-- 	return result
+-- end
+-- NOTE: This only shows the last directory in cwd instead of full path
 function M:update_status()
-    local result = vim.fn.getcwd()
-    if self.options.substitute_home then
-        local home = os.getenv("HOME")
-        if home and vim.startswith(result, home) then
-            result = "~" .. result:sub(home:len() + 1)
-        end
-    end
-    return result
+	local cwd = vim.fn.getcwd()
+	return (cwd:match("([^/\\]+)[/\\]*$") or cwd)
 end
 
 return M
