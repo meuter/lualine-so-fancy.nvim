eeda2572e9f8b6ad03fbf7ddbc5e11def2aa9357
greeid <deekshit.bti@gmail.com>
2024-07-21 06:43:49 +0545

    `chore(icon)` : add neovim icon and make the `fancy_mode` show full length text

--- lua/lualine/components/fancy_mode.lua
+++ lua/lualine/components/fancy_mode.lua
@@ -1,24 +1,24 @@
-local M = require('lualine.component'):extend()
+local M = require("lualine.component"):extend()
 
 function M:init(options)
-    options.width = options.width or 3
-    M.super.init(self, options)
+	options.width = options.width or 7
+	M.super.init(self, options)
 end
 
-local function center(text, width)
-    local spaces = width - vim.fn.strdisplaywidth(text)
-    local left = math.floor(spaces / 2)
-    local right = spaces - left
-    return string.rep(" ", left) .. text .. string.rep(" ", right)
+local function center(text, width, icon)
+	local spaces = width - vim.fn.strdisplaywidth(text)
+	local left = math.floor(spaces / 2)
+	local right = spaces - left - 2
+	return string.rep(" ", left) .. icon .. text .. string.rep(" ", right)
 end
 
 function M:update_status()
-    local mode = require("lualine.utils.mode").get_mode()
-    if self.options.width < string.len(mode) then
-        return string.sub(mode, 1, self.options.width)
-    else
-        return center(require("lualine.utils.mode").get_mode(), self.options.width)
-    end
+	local mode = require("lualine.utils.mode").get_mode()
+	if self.options.width < string.len(mode) then
+		return string.sub(mode, 1, self.options.width)
+	else
+		return center(require("lualine.utils.mode").get_mode(), self.options.width, " ")
+	end
 end
 
 return M
